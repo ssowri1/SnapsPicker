9f8d517eeb882d2555202c63f8ee643206e1ff7f
ntbnapp <notitlebrandnew@gmail.com>
2023-11-14 13:54:12 +0530

    first commit

---
+++ build/Debug-iphonesimulator/SnapsPicker.framework/Headers/SnapsPicker.h
@@ -0,0 +1,18 @@
+//
+//  SnapsPicker.h
+//  SnapsPicker
+//
+//  Created by Sowrirajan S on 14/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SnapsPicker.
+FOUNDATION_EXPORT double SnapsPickerVersionNumber;
+
+//! Project version string for SnapsPicker.
+FOUNDATION_EXPORT const unsigned char SnapsPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnapsPicker/PublicHeader.h>
+
+
