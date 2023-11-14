4f7e42fa32dceca8a938ebc0ea099fc615003cab
ntbnapp <notitlebrandnew@gmail.com>
2023-11-14 18:35:13 +0530

    Added SwiftyLib.podspec

---
+++ SnapsPicker/SnapsPiker.swift
@@ -0,0 +1,17 @@
+//
+//  SnapsPiker.swift
+//  SnapsPicker
+//
+//  Created by Sowrirajan S on 14/11/23.
+//
+
+import Foundation
+
+final class SnapsPicker {
+    
+    let name = "SnapsPicker"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ build/SnapsPicker.build/Debug-iphonesimulator/SnapsPicker.build/Objects-normal/x86_64/SnapsPicker_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/sowrirajans/Documents/Preliminaries/CocoaPods/SnapsPicker/build/SnapsPicker.build/Debug-iphonesimulator/SnapsPicker.build/DerivedSources/SnapsPicker_vers.c
